a1b72d5c3ab7fb68c20a228e8afd99ab70ad99c6
ThienBk <88071364+ThienBk@users.noreply.github.com>
2024-01-17 15:14:45 +0700

    Update home.jsx

--- src/components/router/home.jsx
+++ src/components/router/home.jsx
@@ -3,8 +3,19 @@ import React from "react";
 export default function Home() {
   return (
     <div>
+      <div className="introduce_header">
+        <h1>Hello my name is Thiện</h1>
+        <h2>This is my introduce proflie</h2>
+      </div>
 
-      <h1>Home Page</h1>
+      <div className="proflie">
+        <h3>Full name: Nguyễn Anh Thiện</h3>
+        <h3>Gender: Male</h3>
+        <h3>Phone: 0378889755</h3>
+        <h3>Birthday: 02/01/2000</h3>
+        <h3>Github: https://github.com/ThienBk</h3>
+        <h3>Email address: thiennguyen2000qwer@gmail.com</h3>
+      </div>
     </div>
   );
 }
